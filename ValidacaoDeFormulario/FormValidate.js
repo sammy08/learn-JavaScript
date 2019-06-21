f7f9d25740c52e205d5166358048e2b3621be854
Samuel Santana <35262710+turan-98@users.noreply.github.com>
2019-06-21 17:27:53 -0300

    Create FormValidate.js

---
+++ ValidacaoDeFormulario/FormValidate.js
@@ -0,0 +1,34 @@
+// Neste script ocorre a validação de campos de textos
+
+//variaveis 
+        var button = document.getElementById("btn");
+        
+        //addEventListene
+        button.addEventListener("click",valida,false)
+        
+        //function
+        function valida(){
+            
+            //variaveis
+            var user = document.forms["myForm"]["user"].value;
+            var email = document.forms["myForm"]["email"].value;
+            var senha = document.forms["myForm"]["senha"].value;
+            //fim variaveis
+            
+            //if e else
+            if(user === ""){
+                document.getElementById("response").innerHTML = "campo nome está vazio";
+                document.getElementById("btn").disabled = true;
+                return false;
+            }
+            else if(email === ""){
+                document.getElementById("response").innerHTML = "campo email está vazio";
+                return false;
+            }
+            else if(senha === ""){
+                document.getElementById("response").innerHTML = "campo senha está vazio";
+                return false;
+            }
+            //fim if else
+        }
+    
