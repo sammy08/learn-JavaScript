88fe36c604c3ef9ef48fba8ffcbb23f038b8b0ff
Samuel Santana <35262710+turan-98@users.noreply.github.com>
2019-09-26 12:06:27 -0300

    Add files via up load

---
+++ magicBall.js
@@ -0,0 +1,36 @@
+var userQuestion = 'Question String',
+    randomNumber = Math.floor(Math.random()* 8),
+    eightBall = "Magic Eight Ball's answer ";
+		userQuestion = console.log('Sorte no Amor ??');
+switch (randomNumber){
+  case 0:
+  console.log(eightBall + 'It is certain');
+  break;
+  case 1:
+  console.log(eightBall + "é decididamente assim");
+  break;
+  case 2:
+  console.log(eightBall + ' resposta Hazy tentar novamente ');
+  break;
+  case 3:
+  console.log(eightBall + ' não pode prever agora ');
+  break;
+  case 4:
+  console.log(eightBall + "não conte com isso");
+  break;
+  case 5:
+  console.log( eightBall + "minhas fontes dizem não");
+  break;
+  case 6:
+  console.log(eightBall + ' saida não tão bom ');
+  break;
+  case 7:
+  console.log(eightBall + ' sinais apontam para Sim ');
+  break;
+  case 8:
+  console.log(eightBall + ' sinais apontam para Sim ');
+  break;
+}
+
+
+
